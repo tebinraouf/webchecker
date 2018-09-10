75aa5ba5362e4902d3422fecf5593a80b3177f92
Tebin <tebin.raouf@gmail.com>
2018-09-09 22:46:19 -0400

    add new rule for img and picture tag

--- org.eclipse.epsilon.emc.webchecker/src/org/eclipse/epsilon/webchecker/WebCheckerPropertyGetter.java
+++ org.eclipse.epsilon.emc.webchecker/src/org/eclipse/epsilon/webchecker/WebCheckerPropertyGetter.java
@@ -2,12 +2,8 @@ package org.eclipse.epsilon.webchecker;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.introspection.java.JavaPropertyGetter;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Evaluator;
 
-class Type {
-	public boolean has(String attributeName) {
-		return true;
-	}
-}
 
 class Classes {
 	private Element element;
@@ -16,38 +12,70 @@ class Classes {
 	}
 	
 	public boolean includes(String className) {
-		if (element.hasClass(className) || element.parent().hasClass(className)) {
-			return true;
-		} else {
-			return false;
+		if (className.endsWith("-*")) {
+			String cn = className.substring(0, className.length() - 1);
+			for (String c : element.classNames()) {
+				return c.startsWith(cn);	 
+			}
 		}
+		return (element.hasClass(className) || element.parent().hasClass(className));
 	}
 }
 
 class Parent {
 	private Classes classes;
+	private Element element;
 	public Parent(Element element) {
+		this.element = element;
 		classes = new Classes(element);
 	}
 	
 	public Classes getClasses() {
 		return classes;
 	}
+	/**
+	 * Return <code>true</code> if element parent is the specified tag.
+	 * @param tagName The tag name to check
+	 * @return
+	 */
+	public boolean is(String tagName) {
+		return element.parent().tagName().equals(tagName);
+	}
 }
 
 
 class GuardedElement {
-	public Element element; 
-	 
+	private Element element;
+	private Classes classes;
+	
 	GuardedElement(Element element) {
 		this.element = element;
+		classes = new Classes(element);
 	}
-	public boolean includes(String className) {	
-		if (element.hasClass(className)) {
-			return true;
-		} else {
-			return false;
-		}
+	/**
+	 * Returns true if the element includes the class name, false otherwise. Supports generic class names such as <b>img-*</b>, where <b>*</b> is a wild card.
+	 * @param className The class name to check.
+	 * @return
+	 */
+	public boolean includes(String className) {		
+		if (className.endsWith("-*")) {
+			String cn = className.substring(0, className.length() - 1);
+			for (String c : element.classNames()) {
+				return c.startsWith(cn);	 
+			}
+		} 
+		return (element.hasClass(className));
+	}
+	public Classes getClasses() {
+		return classes;
+	}
+	
+	/**
+	 * Convenient method to guard the element class. Always return true.
+	 * @return <code>true</code>
+	 */
+	public boolean exists() {
+		return true;
 	}
 }
 
@@ -55,9 +83,6 @@ public class WebCheckerPropertyGetter extends JavaPropertyGetter {
 
 	@Override
 	public Object invoke(Object object, String property) throws EolRuntimeException {
-		
-//		System.out.println("Entering invoke() in WebCheckerPropertyGetter class");
-		
 		Element element = (Element) object;
 		
 		if (property.equals("class")) {
